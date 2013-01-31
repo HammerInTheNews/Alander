a82fe4aca3efdd84c2a663da04bf68ccd71de3fc
ajudson <alanjudson@gmail.com>
2013-01-31 18:40:46 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alander::Application.config.secret_token = '094c2439a49e8d1b073006851d9df27e833f18d1cc173d7551e5eaafa451a28426e1ef0b61ca6f6c0ccef659497f906d08721a4aacb229107f86235c231aba7b'
---
+++ db/migrate/20130129011037_make_diary_entry.rb
@@ -0,0 +1,12 @@
+class MakeDiaryEntry < ActiveRecord::Migration
+  def change
+  	create_table :diary_entries do |t|
+  		t.string :title
+  		t.text :description
+  		t.string :mood
+
+  		t.timestamps
+  	end
+
+  end
+end
